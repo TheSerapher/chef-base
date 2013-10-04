d210a7e9c8af44e4b383700cf1122610634653fd
Sebastian Grewe <sebastian@grewe.ca>
2013-10-04 16:47:12 +0200

    [FEATURE] Added Icinga Client

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email  "sebastian.grewe@gmail.com"
 license           "Apache 2.0"
 description       "Installs various cookbooks used by a basic node."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+version           "0.1.1"
 recipe            "base::default", "Installs various software using other cookbooks."
 
 %w( debian ubuntu centos rhel ).each do |sup|
   supports sup
 end
 
-%w( up2date ntp chef-client sudo openssh users ).each do |dep|
+%w( icinga up2date ntp chef-client sudo openssh users ).each do |dep|
   depends dep
 end
