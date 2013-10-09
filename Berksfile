276f1a6556b142115574c8e0ddda8281fd9535d2
Sebastian Grewe <sebastian@grewe.ca>
2013-10-09 23:23:16 +0200

    [IMPROVED] Cookbook Tests
    * Added rubocop tests
    * Fixed icinga in Berkshelf
    * Updated Gemfile
    * Updated Rakefile
    * Added rubocop configuration
    * Fixed metadata for rubocop

--- Berksfile
+++ Berksfile
@@ -2,4 +2,7 @@
 
 metadata
 
+cookbook 'icinga',        :git => 'git://github.com/Bigpoint/icinga.git'
+cookbook 'pnp4nagios',    :git => 'git://github.com/Bigpoint/pnp4nagios.git'
+cookbook 'rrdcached',     :git => 'git://github.com/Bigpoint/rrdcached.git'
 cookbook 'up2date',       :git => 'git://github.com/TheSerapher/chef-up2date.git'
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name              "base"
-maintainer        "Sebastian Grewe"
-maintainer_email  "sebastian.grewe@gmail.com"
-license           "Apache 2.0"
-description       "Installs various cookbooks used by a basic node."
+name              'base'
+maintainer        'Sebastian Grewe'
+maintainer_email  'sebastian.grewe@gmail.com'
+license           'Apache 2.0'
+description       'Installs various cookbooks used by a basic node.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.1"
-recipe            "base::default", "Installs various software using other cookbooks."
+version           '0.1.1'
+recipe            'base::default', 'Installs various software using other cookbooks.'
 
 %w( debian ubuntu centos rhel ).each do |sup|
   supports sup
